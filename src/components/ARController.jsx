7f0f2fc3462c2467acc3d02b5425a6b68895d3c4
Minlync <93282772+Minlync@users.noreply.github.com>
2024-06-10 09:42:10 -0400

    AR lens

--- src/components/ARController.jsx
+++ src/components/ARController.jsx
@@ -18,7 +18,7 @@ const ControlCrytal = () => {
           {/* <p className='text-white'>Scan QR code to experience</p> */}
           {/* <img className="w-[120px] h-[120px] mt-10" src={Crystalcode} alt="QR Code" /> */}
           <a href="https://lens.snap.com/experience/d3b927bc-0ab2-4d4b-b8fe-929b439e6fa6" target="_blank" rel="noopener noreferrer">
-          <button class='bg-gradient-to-r from-[#af46e0] to-[#8d6eff] hover:bg-[#9558d1] w-[200px] rounded-md my-8 font-medium  py-4 text-white duration-300 ease-in-out'>Try ARLense</button>
+          <button class='bg-gradient-to-r from-[#af46e0] to-[#8d6eff] hover:bg-[#9558d1] w-[200px] rounded-md my-8 font-medium  py-4 text-white duration-300 ease-in-out'>Try AR Lens</button>
           </a>
         </div>
         <div className='flex justify-center'>
